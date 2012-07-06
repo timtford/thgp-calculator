f4d2706200886d4a1fb54382c951dc18a5a1ab71
timothy <timothy@labs.hiddengeniusproject.org>
2012-07-06 15:59:00 -0700

    first commit

---
+++ thgp-calculator/thgp-calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  thgp-calculator
+//
+//  Created by Timothy Ford on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ thgp-calculator/thgp-calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  thgp-calculator
+//
+//  Created by Timothy Ford on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
