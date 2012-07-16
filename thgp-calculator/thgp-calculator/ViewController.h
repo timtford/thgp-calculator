19c0ffa25810fd24ecf1f0bfdd53a31c75b2e462
timothy <timothy@labs.hiddengeniusproject.org>
2012-07-16 16:14:56 -0700

    updated public variables
    here you go

--- thgp-calculator/thgp-calculator/ViewController.h
+++ thgp-calculator/thgp-calculator/ViewController.h
@@ -9,9 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController{
-    
-    
-    
+ 
+    double inputNumber1;
+    double inputNumber2;    
+    NSString *operatorPressed;
+    double calculatedResult;
+    UITextField *calculatorDisplay
 }
 -(IBAction)numberPressed:(id)sender;
 -(IBAction)negativePositive:(id)sender;
